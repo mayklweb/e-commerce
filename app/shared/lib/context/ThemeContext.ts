0911fa17ec7d519b59c0592140dbe5b6d885a2d8
Muhammad <muhammadevvv@gmail.com>
2026-01-16 16:24:38 +0500

    m

--- app/shared/lib/context/ThemeContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react'
-import { Theme } from '../../const/theme.ts'
-
-export interface ThemeContextProps {
-  theme?: Theme
-  setTheme?: (theme: Theme) => void
-}
-
-export const ThemeContext = createContext<ThemeContextProps>({})
