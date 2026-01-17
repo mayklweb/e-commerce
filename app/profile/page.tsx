8f1068aa2be7b39ce8b0d90902fee61f786ea517
Muhammad <muhammadevvv@gmail.com>
2026-01-17 14:28:08 +0500

    m

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,4 @@
 import { Button } from "@/components/ui/button";
-
-import { Input } from "@/components/ui/input";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 function Profile() {
@@ -10,11 +8,18 @@ function Profile() {
         <div className="container">
           <div className="flex w-full max-w-sm flex-col gap-6">
             <Tabs className="w-full" defaultValue="account">
-              <TabsList className="w-full">
-                <TabsTrigger value="account">Account</TabsTrigger>
-                <TabsTrigger value="orders">Orders</TabsTrigger>
+              <TabsList className="w-full h-auto p-1">
+                <TabsTrigger className="p-2" value="account">Account</TabsTrigger>
+                <TabsTrigger className="p-2" value="orders">Orders</TabsTrigger>
               </TabsList>
-              <TabsContent value="account">Account</TabsContent>
+              <TabsContent value="account">
+                <div className="flex flex-col gap-4 mt-4">
+                  <input placeholder="Name" />
+                  <input placeholder="Email" />
+                  <input placeholder="Password" type="password" />
+                  <Button className="mt-2">Update Profile</Button>
+                </div>
+              </TabsContent>
               <TabsContent value="orders">Orders</TabsContent>
             </Tabs>
           </div>
