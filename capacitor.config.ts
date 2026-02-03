39e6244b9084b6c8f8a4fd728abdcb05db78ad78
Muhammad <muhammadevvv@gmail.com>
2026-02-03 15:47:15 +0500

    m

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bunyodoptom.app',
+  appName: 'bunyodoptom',
+  webDir: 'public'
+};
+
+export default config;
