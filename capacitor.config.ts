5093328adb72d1d31b97b55eb9a80740be213fd1
Muhammad <muhammadevvv@gmail.com>
2026-02-04 12:17:30 +0500

    m

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,12 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.bunyodoptom.app',
-  appName: 'bunyodoptom',
-  webDir: 'public'
+  appId: "com.bunyodoptom.app",
+  appName: "Bunyod Optom",
+  webDir: "public",
+  server: {
+    url: "https://bunyodoptom.uz"
+  }
 };
 
 export default config;
