651a0e488b32d4606668d01924be6cfe4e9b0c98
Muhammad <muhammadevvv@gmail.com>
2026-03-12 16:37:09 +0500

    cart

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -70,84 +70,84 @@ function Cart() {
                   {cart.map((item) => (
                     <div
                       key={item.id}
-                      className={`bg-white border rounded-xl p-2 flex gap-2 shadow-sm transition-all ${
+                      className={`bg-white border rounded-xl p-2 flex flex-col gap-2 shadow-sm transition-all ${
                         selectedIds.includes(item.id)
                           ? "border-primary/30"
                           : "border-gray-100"
                       }`}
                     >
-                      {/* Image */}
-                      <div className="w-25 h-18.5 rounded-xl bg-gray-100 overflow-hidden shrink-0">
-                        <Image
-                          src={
-                            typeof item.images?.[0] === "string"
-                              ? item.images?.[0]
-                              : item.images?.[0]?.url || "/product.jpg"
-                          }
-                          alt={item.name}
-                          width={300}
-                          height={225}
-                        />
+                      <div className="flex gap-2">
+                        {/* Image */}
+                        <div className="w-25 h-18.5 rounded-xl bg-gray-100 overflow-hidden shrink-0">
+                          <Image
+                            src={
+                              typeof item.images?.[0] === "string"
+                                ? item.images?.[0]
+                                : item.images?.[0]?.url || "/product.jpg"
+                            }
+                            alt={item.name}
+                            width={300}
+                            height={225}
+                          />
+                        </div>
+                        {/* Info */}
+                        <div className="flex-1 min-w-0 flex flex-col">
+                          <p className="text-lg font-semibold text-gray-800 truncate">
+                            {item.name}
+                          </p>
+                          <p className="text-base font-medium text-gray-900">
+                            {item?.price?.toLocaleString()} so'm
+                          </p>
+                          <p className="text-xs">Yetkazib berish vaqti: 1 ish kuni</p>
+                        </div>
+                        {/* Checkbox */}
+                        <div
+                          className="cursor-pointer shrink-0"
+                          onClick={() => toggleItem(item.id)}
+                        >
+                          <div
+                            className={`w-5 h-5 rounded-md border-2 flex items-center justify-center transition-colors ${
+                              selectedIds.includes(item.id)
+                                ? "bg-primary border-primary"
+                                : "border-gray-300 bg-white"
+                            }`}
+                          >
+                            {selectedIds.includes(item.id) && (
+                              <Check className="w-3 h-3 text-white" />
+                            )}
+                          </div>
+                        </div>
                       </div>
 
-                      {/* Info */}
-                      <div className="flex-1 min-w-0 flex flex-col">
-                        <p className="text-lg font-semibold text-gray-800 truncate">
-                          {item.name}
-                        </p>
-                        <p className="text-base font-medium text-gray-900">
-                          {item?.price?.toLocaleString()} so'm
-                        </p>
-                        <p>Yetkazib berish vaqti: 1 ish kuni</p>
+                      <div className="border border-solid border-accent "></div>
 
-                        <div className="border border-solid border-accent mt-2"></div>
-
-                        {/* Qty + Remove */}
-                        <div className="flex gap-4 items-center justify-end mt-2">
-                          <div className="flex items-center gap-2 bg-gray-100 rounded-xl px-1 py-1">
-                            <button
-                              onClick={() => changeQty(item.id, -1)}
-                              className="w-8 h-8 rounded-lg bg-white shadow-sm flex items-center justify-center text-gray-600 hover:text-primary transition-colors cursor-pointer"
-                            >
-                              <Minus className="w-5 h-5" />
-                            </button>
-                            <span className="text-sm font-semibold w-5 text-center">
-                              {item.count}
-                            </span>
-                            <button
-                              onClick={() => changeQty(item.id, 1)}
-                              className="w-8 h-8 rounded-lg bg-white shadow-sm flex items-center justify-center text-gray-600 hover:text-primary transition-colors cursor-pointer"
-                            >
-                              <Plus className="w-5 h-5" />
-                            </button>
-                          </div>
-
+                      {/* Qty + Remove */}
+                      <div className="flex gap-4 items-center justify-end ">
+                        <div className="flex items-center gap-2 bg-gray-100 rounded-xl px-1 py-1">
                           <button
-                            onClick={() => remove(item.id)}
-                            className="flex items-center font-medium gap-2 px-2 py-1.5 bg-error-foreground text-error rounded-lg cursor-pointer"
+                            onClick={() => changeQty(item.id, -1)}
+                            className="w-8 h-8 rounded-lg bg-white shadow-sm flex items-center justify-center text-gray-600 hover:text-primary transition-colors cursor-pointer"
                           >
-                            <Trash2 className="w-5 h-5" />
-                            <span>Yoq qilish</span>
+                            <Minus className="w-5 h-5" />
+                          </button>
+                          <span className="text-sm font-semibold w-5 text-center">
+                            {item.count}
+                          </span>
+                          <button
+                            onClick={() => changeQty(item.id, 1)}
+                            className="w-8 h-8 rounded-lg bg-white shadow-sm flex items-center justify-center text-gray-600 hover:text-primary transition-colors cursor-pointer"
+                          >
+                            <Plus className="w-5 h-5" />
                           </button>
                         </div>
-                      </div>
 
-                      {/* Checkbox */}
-                      <div
-                        className="cursor-pointer shrink-0"
-                        onClick={() => toggleItem(item.id)}
-                      >
-                        <div
-                          className={`w-5 h-5 rounded-md border-2 flex items-center justify-center transition-colors ${
-                            selectedIds.includes(item.id)
-                              ? "bg-primary border-primary"
-                              : "border-gray-300 bg-white"
-                          }`}
+                        <button
+                          onClick={() => remove(item.id)}
+                          className="flex items-center font-medium gap-2 px-2 py-1.5 bg-error-foreground text-error rounded-lg cursor-pointer"
                         >
-                          {selectedIds.includes(item.id) && (
-                            <Check className="w-3 h-3 text-white" />
-                          )}
-                        </div>
+                          <Trash2 className="w-5 h-5" />
+                          <span>Yoq qilish</span>
+                        </button>
                       </div>
                     </div>
                   ))}
