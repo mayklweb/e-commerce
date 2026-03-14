85b77b9f9baaa50606d30ac469fdf44909471f01
Muhammad <muhammadevvv@gmail.com>
2026-03-14 13:35:18 +0500

    m

--- app/components/ProductCard/ProductCard.tsx
+++ app/components/ProductCard/ProductCard.tsx
@@ -2,6 +2,7 @@ import { FavoriteIcon } from "@/app/shared/icons";
 import { useFavoritesStore } from "@/app/store/favoritesStore";
 import { ProductsType } from "@/app/types";
 import Image from "next/image";
+import Link from "next/link";
 
 interface Props {
   product: ProductsType;
@@ -14,43 +15,45 @@ function ProductCard({ product, onClick }: Props) {
   const liked = isFavorite(product.id);
 
   return (
-    <div
-      onClick={onClick}
-      key={product.id}
-      className="flex flex-col items-start gap-5 border-0 shadow-none"
-    >
-      <div className="w-full relative">
-        <button
-          onClick={() => toggleFavorite(product)}
-          className="p-1 absolute top-2 right-2 z-2 bg-white rounded-full"
-        >
-          <FavoriteIcon
-            className={`w-5 h-5 transition-colors ${
-              liked ? "fill-error text-error" : "text-black"
-            }`}
-          />
-        </button>
-        <div className="rounded-xl overflow-hidden mb-2.5">
-          <Image
-            width={300}
-            height={225}
-            src={"/product.jpg"}
-            alt={product.name}
-            className="w-full h-full object-cover"
-          />
-        </div>
+    // <Link href={`${product.slug}`}>
+      <div
+        onClick={onClick}
+        key={product.id}
+        className="flex flex-col items-start gap-5 border-0 shadow-none cursor-pointer"
+      >
+        <div className="w-full relative">
+          <button
+            onClick={() => toggleFavorite(product)}
+            className="p-1 absolute top-2 right-2 z-2 bg-white rounded-full"
+          >
+            <FavoriteIcon
+              className={`w-5 h-5 transition-colors ${
+                liked ? "fill-error text-error" : "text-black"
+              }`}
+            />
+          </button>
+          <div className="rounded-xl overflow-hidden mb-2.5">
+            <Image
+              width={300}
+              height={225}
+              src={"/product.jpg"}
+              alt={product.name}
+              className="w-full h-full object-cover"
+            />
+          </div>
 
-        <div className="flex flex-col items-start gap-0.5 w-full">
-          <h2 className="w-full font-semibold text-black text-base tracking-tight leading-[100%] truncate ">
-            {product.name}
-          </h2>
+          <div className="flex flex-col items-start gap-0.5 w-full">
+            <h2 className="w-full font-semibold text-black text-base tracking-tight leading-[100%] truncate ">
+              {product.name}
+            </h2>
 
-          <p className="font-semibold text-[#000000cc] text-base tracking-tight">
-            {product.price?.toLocaleString()} so'm
-          </p>
+            <p className="font-semibold text-[#000000cc] text-base tracking-tight">
+              {product.price?.toLocaleString()} so'm
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    // </Link>
   );
 }
 
--- app/home/ui/sections/RecomendedProducts.tsx
+++ app/home/ui/sections/RecomendedProducts.tsx
@@ -1,4 +1,6 @@
 import { ProductCard } from "@/app/components";
+import ProductModal from "@/app/components/ProductModal/ProductModal";
+import { useProductModal } from "@/app/shared/lib/hooks/useProductModal";
 import { useProducts } from "@/app/shared/lib/hooks/useProducts";
 import { useShuffledProducts } from "@/app/shared/lib/hooks/useShuffledProducts";
 import { ProductsType } from "@/app/types";
@@ -6,6 +8,7 @@ import { ProductsType } from "@/app/types";
 function RecomendedProducts() {
   const { data: products, isLoading, isError } = useProducts();
   const recomendedProduct = useShuffledProducts<ProductsType>(products, 20);
+  const { selectedProduct, openModal, closeModal } = useProductModal();
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error</div>;
@@ -21,11 +24,16 @@ function RecomendedProducts() {
           </div>
           <div className="mt-6 mb-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
             {recomendedProduct.map((product: ProductsType, i) => (
-              <ProductCard key={i} product={product} />
+              <ProductCard
+                key={i}
+                product={product}
+                onClick={() => openModal(product)}
+              />
             ))}
           </div>
         </div>
       </div>
+      <ProductModal product={selectedProduct} onClose={closeModal} />
     </section>
   );
 }
