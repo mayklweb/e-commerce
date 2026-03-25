5f6565cdbda33b5419d4c444c4fe12abd2e44e34
Muhammad <muhammadevvv@gmail.com>
2026-03-25 16:48:44 +0500

    m

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
  images: {
-  remotePatterns: [
-    { protocol: 'https', hostname: 'bunyodoptom.uz' }
-  ]
-}
+    remotePatterns: [
+      { protocol: 'https', hostname: 'api.bunyodoptom.uz' }
+    ]
+  }
 };
 
 export default nextConfig;
